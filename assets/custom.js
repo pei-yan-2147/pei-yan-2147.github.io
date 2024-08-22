59d043c892a65ed4463088a718d6380f6684eec9
pai-yan <x10640305@gmail.com>
2024-08-23 01:42:35 +0800

    re

---
+++ assets/custom.js
@@ -0,0 +1,11 @@
+function copyToClipboard(element) {
+  var text = document.querySelector(element).innerText;
+  var textarea = document.createElement("textarea");
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
+  alert("已複製到剪貼簿");
+}
+

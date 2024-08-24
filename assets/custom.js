2868c7522651cc4eedc2824119c327847772e1dd
pai-yan <x10640305@gmail.com>
2024-08-25 00:14:40 +0800

    Deployed 12ff859 with MkDocs version: 1.6.0

--- assets/custom.js
+++ assets/custom.js
@@ -1,11 +1,15 @@
 function copyToClipboard(element) {
+  console.log('Button clicked, starting copy process');  // 添加這一行來測試
   var text = document.querySelector(element).innerText;
   var textarea = document.createElement("textarea");
   textarea.value = text;
   document.body.appendChild(textarea);
   textarea.select();
-  document.execCommand("copy");
+  var successful = document.execCommand("copy");
   document.body.removeChild(textarea);
-  alert("已複製到剪貼簿");
+  if (successful) {
+      alert("已複製到剪貼簿");
+  } else {
+      alert("複製失敗，請手動選取並複製");
+  }
 }
-  
